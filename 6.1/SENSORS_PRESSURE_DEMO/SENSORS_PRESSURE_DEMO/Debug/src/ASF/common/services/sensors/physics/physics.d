46fdc47ed9563618e319f4c6a78c32cde14948be
THXC <cap.price.wow@gmail.com>
2015-04-25 10:07:00 +0200

    New
    Atmel Studio

---
+++ 6.1/SENSORS_PRESSURE_DEMO/SENSORS_PRESSURE_DEMO/Debug/src/ASF/common/services/sensors/physics/physics.d
@@ -0,0 +1,33 @@
+src/ASF/common/services/sensors/physics/physics.d \
+ src/ASF/common/services/sensors/physics/physics.o: \
+ ../src/ASF/common/services/sensors/physics/physics.c \
+ ../src/ASF/common/services/sensors/physics/physics.h \
+ ../src/ASF/common/services/sensors/physics/constants.h \
+ ../src/ASF/common/services/sensors/physics/../math/precision.h \
+ ../src/ASF/common/services/sensors/physics/../math/fixed.h \
+ ../src/ASF/common/services/sensors/physics/../math/fixed_t.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/assert.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdbool.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/math.h
+
+../src/ASF/common/services/sensors/physics/physics.h:
+
+../src/ASF/common/services/sensors/physics/constants.h:
+
+../src/ASF/common/services/sensors/physics/../math/precision.h:
+
+../src/ASF/common/services/sensors/physics/../math/fixed.h:
+
+../src/ASF/common/services/sensors/physics/../math/fixed_t.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/assert.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdbool.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/math.h:
